f2c1f2fe202906c6b91b5f09ed8d51b4ee13513f
kriper12 <ilinivan1602@gmail.com>
2022-01-17 19:44:04 +0300

    добавлен зомби, c минимальным  ИИ

--- sprites/Zombie.py
+++ sprites/Zombie.py
@@ -6,9 +6,32 @@ from constants import *
 
 
 class Zombie(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, player):
         pygame.sprite.Sprite.__init__(self)
+        self.player = player
         self.image = pygame.Surface((50, 50))
         self.image.fill(RED)
         self.rect = self.image.get_rect()
-        self.rect.center = (random.randint(0,WIDTH), random.randint(0, HEIGHT))
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+        self.speedx = 0
+        self.speedy = 0
+
+    def update(self):
+
+        one_control = random.randint(0,5)
+        if one_control == 1:
+            two_control = random.randint(0,3)
+            if two_control == 0:
+                self.speedx -= 1
+            elif two_control == 1:
+                self.speedx += 1
+            elif two_control == 2:
+                self.speedy -= 1
+            elif two_control == 3:
+                self.speedy += 1
+        self.rect.y += self.speedy
+        self.rect.x += self.speedx
+        self.a = random.randint(0,14)
+        if  self.a == 1:
+            self.speedx = 0
+            self.speedy = 0
